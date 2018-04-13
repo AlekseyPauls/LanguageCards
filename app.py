29ddd56ad953b09d64d2c3666708bae7c33dc1a1
AlekseyPauls <aleksey.pauls@mail.ru>
2018-04-14 00:06:34 +0700

    Made interface easier and more convenient

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, send_file
+from flask import Flask, render_template, redirect, url_for, send_file, abort
 from flask_socketio import SocketIO, join_room, leave_room, rooms
 import random
 import os
 
 from mongoengine import *
-from models import Card, Deck
+from models import Room, Deck
 
 
 app = Flask(__name__)
@@ -29,23 +29,7 @@ MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://%(host)s:%(port)s/%(db)s' % M
 db = connect(MONGO['db'], host=MONGO['host'])
 
 
-rooms = {}
-
-
 """ Routing """
-
-
-@app.route('/cards/<deck_name><int:card>')
-def cards(deck_name, card):
-    deck = Deck.objects(Q(name=deck_name)).first()
-    card = deck.cards[card]
-    return send_file(card.image, mimetype='image')
-
-
-@app.route('/base_card')
-def base_card():
-    base_card = open('static/1.png', 'rb')
-    return send_file(base_card, mimetype='image')
 
 
 @app.route('/')
@@ -53,9 +37,12 @@ def start():
     return render_template('./StartPage.html')
 
 
-@app.route('/room/<int:room_id>')
+@app.route('/room/<room_id>')
 def room(room_id):
-    return render_template('./RoomPage.html', room_id=room_id)
+    if room_exists(room_id):
+        return render_template('./RoomPage.html', room_id=room_id)
+    else:
+        abort(404)
 
 
 @app.route('/create_deck')
@@ -73,25 +60,21 @@ def edit_deck(name):
 
 @socketio.on('send table')
 def send_table(room):
-    deck_list = []
-    for deck in Deck.objects():
-        deck_list.append({'name': deck.name, 'length': deck.length, 'description': deck.description})
-    print(deck_list)
+    deck_list = get_deck_list()
     socketio.emit('make table', deck_list, room=room)
 
 
 @socketio.on('start game')
 def start_game(data):
-    global rooms
-    new_room = data['new room']
-    room = data['current room']
-    room_state = {}
-    room_state['deck_name'] = data['deck_name']
-    room_state['clients'] = 0
-    room_state['current card'] = random_card(data['deck_name'])
-    rooms[new_room] = room_state
-    #print('Redirect to new room: ' + str(new_room) + '   ' + str(room))
-    socketio.emit('redirect', url_for('room', room_id=new_room), room=room)
+    room = data['current_room']
+    new_room = {}
+    new_room['id'] = generate_room_id()
+    new_room['decks'] = data['decks']
+    new_room['clients'] = 0
+    add_room(new_room)
+    get_random_cards(new_room['id'])
+    app.logger.debug('Created game room with id: ' + str(new_room['id']))
+    socketio.emit('redirect', url_for('room', room_id=new_room['id']), room=room)
 
 
 @socketio.on('redirect to CreateDeck')
@@ -105,15 +88,24 @@ def redirect_to_CreateDeck(room):
 
 @socketio.on('save deck')
 def save_deck(data):
+    print(data)
     room = data['room']
     deck = {}
     deck['name'] = data['name']
     deck['description'] = data['description']
-    deck['length'] = 0
+    deck['cards'] = []
+
+    if data.get('cards') is not None:
+        deck['cards'].extend(parse_cards(data['cards'], 'string'))
+
+    if data.get('file') is not None:
+        deck['cards'].extend(parse_cards(data['file'], 'file'))
+
+    deck['length'] = len(deck['cards'])
 
     res = save_deck_to_db(deck)
     if res:
-        socketio.emit('redirect', url_for('edit_deck', name=deck['name']), room=room)
+        socketio.emit('message', 'Deck was saved!', room=room)
     else:
         socketio.emit('message', 'Can`t save deck: name is already using', room=room)
 
@@ -122,56 +114,55 @@ def save_deck(data):
 """ Edit deck page """
 
 
-@socketio.on('save card')
-def save_card(data):
-    room = data['room']
-    save_card_to_db(data)
-    deck = Deck.objects(Q(name=data['deck_name'])).first()
-    socketio.emit('get deck info', {'length': deck.length, 'description': deck.description}, room=room)
 
 
 
-""" Room page """\
+
+""" Room page """
 
 
 @socketio.on('join game room')
 def join_game_room(room):
-    global rooms
     join_room(room)
-    (rooms[room])['clients'] += 1
+    app.logger.debug('Join to game room: ' + str(room))
+    increase_clients(room)
     #print('Has entered the game room: ' + str(room) + '. Now clients: ' + str((rooms[room])['clients']))
 
 
 @socketio.on('leave game room')
 def on_leave(room):
-    global rooms
     leave_room(room)
-    (rooms[room])['clients'] -= 1
+    decrease_clients(room)
     #print('Has leaved the room: ' + str(room) + '. Now clients: ' + str((rooms[room])['clients']))
 
 
+# Where it needs?
 @socketio.on('send deck info')
-def send_deck_infok(data):
+def send_deck_info(data):
     room = data['room']
     name = data['name']
     deck = Deck.objects(Q(name=name)).first()
     socketio.emit('get deck info', {'length': deck.length, 'description': deck.description}, room=room)
 
 
-@socketio.on('prepare room')
-def prepare_room(room):
-    card = (rooms[room])['current card']
-    #print('Prepare room: ' + str(room) + '   ' + card)
-    socketio.emit('get new card', {'text': card['text'], 'image': card['image']}, room=room)
+@socketio.on('prepare field')
+def send_decks_count(data):
+    room = Room.objects(Q(name=data['room'])).first()
+    count = len(room.decks)
+    socketio.emit('make field', count, room=data['client'])
+
+# To do: send current cards only to new room
+@socketio.on('prepare cards')
+def prepare_room(data):
+    cards = get_current_cards(data['room'])
+    socketio.emit('get new cards', cards, room=data['client'])
 
 
-@socketio.on('send new card')
-def send_new_card(room):
-    global rooms
-    new_card = random_card((rooms[room])['deck_name'])
-    (rooms[room])['current card'] = new_card
+@socketio.on('send new cards')
+def send_new_cards(room):
+    new_cards = get_random_cards(room)
     #print('New current card for room: ' + str(room) + ' ' + (rooms[room])['current card'])
-    socketio.emit('get new card', {'text': new_card['text'], 'image': new_card['image']}, room=room)
+    socketio.emit('get new cards', new_cards, room=room)
 
 
 """ Common """
@@ -181,10 +172,9 @@ def send_new_card(room):
 def join_single_room(room):
     join_room(room)
     #logging
-    app.logger.debug('A value for debugging')
-    app.logger.warning('A warning occurred (%d apples)', 42)
-    app.logger.error('An error occurred')
-    #print('Has entered the single room: ' + str(room))
+    app.logger.debug('Join to single room: ' + str(room))
+    #app.logger.warning('A warning occurred (%d apples)', 42)
+    #app.logger.error('An error occurred')
 
 
 @socketio.on('redirect to Start')
@@ -196,44 +186,104 @@ def redirect_to_Start(room):
 """ Database using """
 
 
-def random_card(deck_name):
-    deck = Deck.objects(Q(name=deck_name)).first()
-    r = random.randint(0, deck.length - 1)
-    card = deck.cards[r]
-    if card.image:
-        return {'text': card.text, 'image': url_for('cards', deck_name=deck_name, card=r)}
+def room_exists(room_id):
+    print("Exists room " + room_id)
+    if Room.objects(Q(name=room_id)).first() is not None:
+        return True
     else:
-        return {'text': card.text, 'image': ''}
+        return False
+
+
+def increase_clients(room_id):
+    room = Room.objects(Q(name=room_id)).first()
+    room.clients += 1
+    room.save()
+
+
+def decrease_clients(room_id):
+    room = Room.objects(Q(name=room_id)).first()
+    room.clients -= 1
+    # close room
+    if room.clients == 0:
+        room.delete()
+    else:
+        room.save()
+
+
+def add_room(room):
+    new_room = Room(name=room['id'], decks=room['decks'],  clients=room['clients'])
+    new_room.save()
+
+
+def get_random_cards(room_id):
+    room = Room.objects(Q(name=room_id)).first()
+    cards = []
+    for deck_name in room.decks:
+        deck = Deck.objects(Q(name=deck_name)).first()
+        card = random_card(deck.cards)
+        cards.append(card)
+    room.current_cards = cards
+    room.save()
+    return cards
+
+
+def get_current_cards(room_id):
+    room = Room.objects(Q(name=room_id)).first()
+    return room.current_cards
 
 
 def get_deck_list():
     deck_list = []
     for deck in Deck.objects():
-        deck_list.append({'name': deck.name, 'length': deck.length})
+        deck_list.append({'name': deck.name, 'length': deck.length, 'description': deck.description})
     return deck_list
 
 
 def save_deck_to_db(deck):
     if Deck.objects(Q(name=deck['name'])).first() is not None:
         return False
-    new_deck = Deck(name=deck['name'], description=deck['description'], length=deck['length'])
+    new_deck = Deck(name=deck['name'], description=deck['description'], length=deck['length'], cards=deck['cards'])
     new_deck.save()
     return True
 
 
-def save_card_to_db(card):
-    new_card = Card(text=card['text'])
-    if card['image'] != '':
-      new_card.image.put(card['image'], content_type = 'image/')
-    deck = Deck.objects(Q(name=card['deck_name'])).first()
-    deck.cards.append(new_card)
-    deck.length += 1
-    deck.save()
+""" Service logic """
 
 
-def get_cards(deck_name):
-    deck = Deck.objects(Q(name=deck_name)).first()
-    return deck.cards
+def parse_cards(cards, mode):
+    ret = []
+    if mode == 'file':
+        tmp = cards.decode('utf-8').split('\n')
+    else :
+        tmp = cards.split('\n')
+    str = ""
+    for line in tmp:
+        if len(line) == 0:
+            continue
+        if line[len(line) - 1] == '/':
+            str = str + line[:len(line) - 1] + '\n'
+        else:
+            if str != "":
+                str = str + line
+                ret.append(str)
+                str = ""
+            else:
+                ret.append(line)
+
+    return ret
+
+
+def generate_room_id():
+    id = random.randint(0, 100000000)
+    while Room.objects(Q(name=id)).first() is not None:
+        id = random.randint(0, 100000000)
+    return id
+
+
+def random_card(cards):
+    r = random.randint(0, len(cards) - 1)
+    return cards[r]
+
 
 
 """ Application """
--- models.py
+++ models.py
@@ -1,13 +1,16 @@
 from mongoengine import *
 
 
-class Card(EmbeddedDocument):
-    image = FileField()
-    text = StringField()
+class Room(Document):
+    name = IntField()
+    decks = ListField()
+    current_cards = ListField()
+    clients = IntField()
+
 
 class Deck(Document):
     #Id = IntField()
     name = StringField()
     description = StringField()
     length = IntField()
-    cards = EmbeddedDocumentListField(Card)
+    cards = ListField()
