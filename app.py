f0f93d5e15b1f8c09b489c261364b1fc8d76c219
AlekseyPauls <aleksey.pauls@mail.ru>
2018-04-19 17:28:37 +0700

    A few changes and bugs fixed

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, url_for, send_file, abort
 from flask_socketio import SocketIO, join_room, leave_room, rooms
 import random
 import os
+import glob
+import re
 
 from mongoengine import *
 from models import Room, Deck
@@ -12,18 +14,18 @@ app.config['SECRET_KEY'] = 'jsbcfsbfjefebw237u3gdbdc'
 socketio = SocketIO(app)
 
 # Test Config
-# MONGO = {
-#                 'db': 'mongo',
-#                 'host': 'localhost',
-#                 'port': '27017',
-# }
-
-#App Config
 MONGO = {
                 'db': 'mongo',
-                'host': 'db',
+                'host': 'localhost',
                 'port': '27017',
 }
+
+#App Config
+# MONGO = {
+#                 'db': 'mongo',
+#                 'host': 'db',
+#                 'port': '27017',
+# }
 
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://%(host)s:%(port)s/%(db)s' % MONGO)
 db = connect(MONGO['db'], host=MONGO['host'])
@@ -52,7 +54,18 @@ def create_deck():
 
 @app.route('/edit_deck/<name>')
 def edit_deck(name):
-    return render_template('./EditDeckPage.html', name=name)
+    if deck_exists(name):
+        return render_template('./EditDeckPage.html', name=name)
+    else:
+        abort(404)
+
+
+@app.route('/decks/<name>')
+def decks(name):
+    deck_name = name[: name.rfind('.')]
+    make_tmp_file(deck_name)
+    file = open('tmp/' + name, 'rb')
+    return send_file(file, mimetype='text/')
 
 
 """ Start page """
@@ -64,6 +77,7 @@ def send_table(room):
     socketio.emit('make table', deck_list, room=room)
 
 
+# Add room_life_time check to delete useless rooms before create new
 @socketio.on('start game')
 def start_game(data):
     room = data['current_room']
@@ -79,8 +93,12 @@ def start_game(data):
 
 @socketio.on('redirect to CreateDeck')
 def redirect_to_CreateDeck(room):
-    room = room['room']
     socketio.emit('redirect', url_for('create_deck'), room=room)
+
+
+@socketio.on('redirect to EditDeck')
+def redirect_to_EditDeck(data):
+    socketio.emit('redirect', url_for('edit_deck', name=data['deck']), room=data['room'])
 
 
 """ Create deck page """
@@ -94,6 +112,10 @@ def save_deck(data):
     deck['name'] = data['name']
     deck['description'] = data['description']
     deck['cards'] = []
+
+    if (not check_deck_name(deck['name'])):
+        socketio.emit('message', 'Bad deck name!', room=room)
+        return
 
     if data.get('cards') is not None:
         deck['cards'].extend(parse_cards(data['cards'], 'string'))
@@ -114,8 +136,36 @@ def save_deck(data):
 """ Edit deck page """
 
 
+@socketio.on('send deck info')
+def send_deck_info(data):
+    room = data['room']
+    name = data['name']
+    print(name)
+    deck = Deck.objects(Q(name=name)).first()
+    socketio.emit('get deck info', {'description': deck.description, 'cards': deck.cards}, room=room)
 
 
+@socketio.on('change deck')
+def change_deck(data):
+    deck = {}
+    deck['name'] = data['deck']
+    deck['cards'] = []
+
+    deck['cards'].extend(parse_cards(data['cards'], 'string'))
+    deck['length'] = len(deck['cards'])
+    change_deck_in_db(deck)
+
+
+@socketio.on('delete deck')
+def delete_deck(name):
+    delete_deck_from_db(name)
+
+
+@socketio.on('clear tmp')
+def clear_tmp():
+    files = glob.glob('tmp/*')
+    for f in files:
+        os.remove(f)
 
 
 """ Room page """
@@ -124,25 +174,13 @@ def save_deck(data):
 @socketio.on('join game room')
 def join_game_room(room):
     join_room(room)
-    app.logger.debug('Join to game room: ' + str(room))
     increase_clients(room)
-    #print('Has entered the game room: ' + str(room) + '. Now clients: ' + str((rooms[room])['clients']))
 
 
 @socketio.on('leave game room')
-def on_leave(room):
+def leave_game_room(room):
     leave_room(room)
     decrease_clients(room)
-    #print('Has leaved the room: ' + str(room) + '. Now clients: ' + str((rooms[room])['clients']))
-
-
-# Where it needs?
-@socketio.on('send deck info')
-def send_deck_info(data):
-    room = data['room']
-    name = data['name']
-    deck = Deck.objects(Q(name=name)).first()
-    socketio.emit('get deck info', {'length': deck.length, 'description': deck.description}, room=room)
 
 
 @socketio.on('prepare field')
@@ -151,7 +189,7 @@ def send_decks_count(data):
     count = len(room.decks)
     socketio.emit('make field', count, room=data['client'])
 
-# To do: send current cards only to new room
+
 @socketio.on('prepare cards')
 def prepare_room(data):
     cards = get_current_cards(data['room'])
@@ -161,7 +199,6 @@ def prepare_room(data):
 @socketio.on('send new cards')
 def send_new_cards(room):
     new_cards = get_random_cards(room)
-    #print('New current card for room: ' + str(room) + ' ' + (rooms[room])['current card'])
     socketio.emit('get new cards', new_cards, room=room)
 
 
@@ -171,7 +208,6 @@ def send_new_cards(room):
 @socketio.on('join single room')
 def join_single_room(room):
     join_room(room)
-    #logging
     app.logger.debug('Join to single room: ' + str(room))
     #app.logger.warning('A warning occurred (%d apples)', 42)
     #app.logger.error('An error occurred')
@@ -179,7 +215,7 @@ def join_single_room(room):
 
 @socketio.on('redirect to Start')
 def redirect_to_Start(room):
-    room = room['room']
+    print('redirect of ' + room)
     socketio.emit('redirect', url_for('start'), room=room)
 
 
@@ -187,25 +223,40 @@ def redirect_to_Start(room):
 
 
 def room_exists(room_id):
-    print("Exists room " + room_id)
     if Room.objects(Q(name=room_id)).first() is not None:
         return True
     else:
         return False
 
 
+def deck_exists(name):
+    if Deck.objects(Q(name=name)).first() is not None:
+        return True
+    else:
+        return False
+
+
+def generate_room_id():
+    id = random.randint(0, 100000000)
+    while Room.objects(Q(name=id)).first() is not None:
+        id = random.randint(0, 100000000)
+    return id
+
+
 def increase_clients(room_id):
     room = Room.objects(Q(name=room_id)).first()
     room.clients += 1
+    app.logger.debug('Join game room: ' + str(room_id) + ". Current number of clients: " + str(room.clients))
     room.save()
 
 
 def decrease_clients(room_id):
     room = Room.objects(Q(name=room_id)).first()
     room.clients -= 1
-    # close room
+    app.logger.debug('Leave game room: ' + str(room_id) + ". Current number of clients: " + str(room.clients))
     if room.clients == 0:
         room.delete()
+        app.logger.debug('Close game room: ' + str(room_id))
     else:
         room.save()
 
@@ -247,7 +298,39 @@ def save_deck_to_db(deck):
     return True
 
 
+def change_deck_in_db(deck):
+    changing_deck = Deck.objects(Q(name=deck['name'])).first()
+    changing_deck.cards = deck['cards']
+    changing_deck.length = deck['length']
+    changing_deck.save()
+
+
+def delete_deck_from_db(deck):
+    deleting_deck = Deck.objects(Q(name=deck)).first()
+    deleting_deck.delete()
+
+
+def make_tmp_file(name):
+    deck = Deck.objects(Q(name=name)).first()
+    file = open('tmp/' + name + '.txt', 'w')
+    descr = deck.description.split('\n')
+    for line in descr:
+        file.write('#' + line + '\n')
+    for line in deck.cards:
+        file.write(line + '\n')
+    return file
+
+
 """ Service logic """
+
+
+
+def check_deck_name(name):
+    if (re.findall(r'[ ^ A - Za - z0 - 9 - _ +:.!]+', name) is ''):
+        return True
+    else:
+        return False
+
 
 
 def parse_cards(cards, mode):
@@ -271,13 +354,6 @@ def parse_cards(cards, mode):
                 ret.append(line)
 
     return ret
-
-
-def generate_room_id():
-    id = random.randint(0, 100000000)
-    while Room.objects(Q(name=id)).first() is not None:
-        id = random.randint(0, 100000000)
-    return id
 
 
 def random_card(cards):
